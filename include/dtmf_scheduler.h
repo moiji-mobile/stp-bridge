b26549d13b1cadfa0755b26008517e8debe37341
Holger Hans Peter Freyther <zecke@selfish.org>
2012-11-29 21:41:01 +0100

    dtmf: Schedule DTMF tones for the MTN hardware
    Create a simple queue for pending DTMF tones, play them using the
    MTN API, and then send the next tones once the playback is complete.
    The callback and scheduling is done from the same context so no locking
    needs to be done.

---
+++ include/dtmf_scheduler.h
@@ -0,0 +1,25 @@
+#ifndef DTMF_SCHEDULER_H
+#define DTMF_SCHEDULER_H
+
+/**
+ * The state/queue for DTMF signalling.
+ */
+struct dtmf_state {
+	int size;		/* <! The last tone to play */
+	char tones[24];		/* <! Pending tones */
+	int playing;		/* <! Playing a tone right now? */
+};
+
+/* initialize */
+void dtmf_state_init(struct dtmf_state *state);
+
+/* add a tone to the list */
+int dtmf_state_add(struct dtmf_state *state, char tone);
+
+/* tones that should be played, playing will be set to 1 */
+void dtmf_state_get_pending(struct dtmf_state *state, char *tones);
+
+/* call when the playout is done */
+void dtmf_state_played(struct dtmf_state *state);
+
+#endif
---
+++ src/dtmf_scheduler.c
@@ -0,0 +1,59 @@
+/*
+ * (C) 2012 by Holger Hans Peter Freyther
+ * (C) 2012 by On-Waves
+ * All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "dtmf_scheduler.h"
+#include <string.h>
+#include <stdio.h>
+
+void dtmf_state_init(struct dtmf_state *state)
+{
+	memset(state, 0, sizeof(*state));
+}
+
+int dtmf_state_add(struct dtmf_state *state, char tone)
+{
+	/* we would override the head */
+	if (state->size == sizeof(state->tones))
+		return -1;
+
+	state->tones[state->size++] = tone;
+	return 0;
+}
+
+void dtmf_state_get_pending(struct dtmf_state *state, char *tones)
+{
+	int pos;
+
+	for (pos = 0; pos < state->size; ++pos)
+		tones[pos] = state->tones[pos];
+
+	/* consume everything up to the tail */
+	state->size = 0;
+
+	/* remember that we play things */
+	if (pos > 0)
+		state->playing = 1;
+	tones[pos] = '\0';
+}
+
+void dtmf_state_played(struct dtmf_state *state)
+{
+	state->playing = 0;
+}
